8d4e5cbe7321590118b99b902db1b5eb1a4900ed
Ashish Kumar Adarsh <89870185+ashishadarsh1308@users.noreply.github.com>
2023-07-07 01:50:13 +0530

    added routers

--- MERN E-com/Backend/controllers/productController.js
+++ MERN E-com/Backend/controllers/productController.js
@@ -1,3 +1,42 @@
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({ message: 'Route is working' })
+const Product = require('../models/productModel')
+// Create new product => /api/v1/admin/product/new
+
+// creaote the product --Admin
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// Get all products => /api/v1/products
+exports.getAllProducts = async (req, res) => {
+    const product = await Product.find();
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// update product --Admin
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
--- MERN E-com/Backend/models/productModel.js
+++ MERN E-com/Backend/models/productModel.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter product name'],
+        trim: true,
     },
     description: {
         type: String,
@@ -13,5 +14,57 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Please enter product price'],
         maxLength: [8, 'Price cannot exceed 8 characters'],
-    }
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this product'],
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxLength: [4, 'Stock cannot exceed 4 characters'],
+        default: 1,
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0,
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: Number,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            },
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
+
+module.exports = mongoose.model('Product', productSchema);
