233b7b6d0799e8de121ebf505843ceb3e1ba278a
Ashish Kumar Adarsh <ashishadarsh319@gmail.com>
2023-07-16 01:01:14 +0530

    adding navbar

--- MERN E-com/frontend/src/App.js
+++ MERN E-com/frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Header from './components/layout/Header/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import WebFont from 'webfontloader';
+
+
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka' ],
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-     asdklfjh
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
